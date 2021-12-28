9aba6e2194004d196e93437964218d3455252e04
Zdenek Hasek <me@zdehasek.com>
2021-12-28 10:51:28 +0100

    gh pages support

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,32 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter(),
+
+// 		// hydrate the <div id="svelte"> element in src/app.html
+// 		target: '#svelte'
+// 	}
+// };
+
+// export default config;
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
-
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		paths: {
+			base: dev ? '' : '/sveltekit',
+		},
+		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+		// For example, instead of '_app', use 'app_', 'internal', etc.
+		appDir: 'internal',
 	}
 };
 
